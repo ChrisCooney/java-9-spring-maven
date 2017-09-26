b6fec05da7ffdec704b5a7d4c2363e3e72dacbdc
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-09-26 21:01:24 +0100

    Added basic working skeleton

---
+++ app/src/main/java/java9/spring/mvc/Java9SpringMvcExample.java
@@ -0,0 +1,13 @@
+package java9.spring.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class Java9SpringMvcExample {
+    public static void main(String[] args) {
+        SpringApplication.run(Java9SpringMvcExample.class, args);
+    }
+}
---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module java9.spring.mvc {
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+}
