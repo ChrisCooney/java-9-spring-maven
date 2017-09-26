c53bc8be55474e15475a1f6c785dcb37f3be2ef1
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-09-26 21:15:57 +0100

    Added users module with controller

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module java9.spring.mvc {
     requires spring.context;
     requires spring.boot.autoconfigure;
     requires spring.boot;
+    requires users;
 }
---
+++ users/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module users {
+    requires spring.web;
+
+    exports java9.spring.mvc.users.api;
+    exports java9.spring.mvc.users.model;
+}
