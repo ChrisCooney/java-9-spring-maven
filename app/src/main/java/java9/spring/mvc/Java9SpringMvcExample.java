37e9accee2f2a23d241f2987aa33865b518b179f
Chris Cooney <chris_cooney@hotmail.co.uk>
2017-09-26 22:03:46 +0100

    Did some jiggery pokery

--- app/src/main/java/java9/spring/mvc/Java9SpringMvcExample.java
+++ app/src/main/java/java9/spring/mvc/Java9SpringMvcExample.java
@@ -1,11 +1,9 @@
 package java9.spring.mvc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Java9SpringMvcExample {
     public static void main(String[] args) {
         SpringApplication.run(Java9SpringMvcExample.class, args);
--- users/src/main/java/java9/spring/mvc/users/api/UsersController.java
+++ users/src/main/java/java9/spring/mvc/users/api/UsersController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(path = "/users")
 @RestController
 public class UsersController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/users/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     public User getUser(@PathVariable("id") String id) {
         System.out.println(String.format("Got request for user with id %s", id));
         return new User(id, "Chris", "His social life is so poor he spends his time talking about himself in code");
