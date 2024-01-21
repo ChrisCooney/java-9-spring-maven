abde94540687491b1a281e840295e82ae3da1b65
Tarandeep Singh <tarandeep.singh562@gmail.com>
2024-01-20 18:02:29 -0800

    jdk-inconsistencies

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -3,8 +3,4 @@ module java9.spring.mvc {
     requires spring.context;
     requires spring.boot.autoconfigure;
     requires spring.boot;
-    requires users;
-    requires admin;
-
-    exports java9.spring.mvc;
 }
